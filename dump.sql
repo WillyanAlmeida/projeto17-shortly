039221dadf909d5c6c118325eed2b5924333bc8d
Willyan <wkinhaamaral@gmail.com>
2023-08-03 14:56:36 -0300

    fix: dump

--- dump.sql
+++ dump.sql
@@ -1,31 +1,164 @@
-CREATE TABLE "public.users" (
-	"id" serial NOT NULL,
-	"name" TEXT NOT NULL,
-	"email" TEXT NOT NULL UNIQUE,
-	"password" TEXT NOT NULL,
-	CONSTRAINT "users_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.8 (Ubuntu 14.8-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.8 (Ubuntu 14.8-0ubuntu0.22.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: public.shortly; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public."public.shortly" (
+    id integer NOT NULL,
+    url character(1) NOT NULL,
+    "shortUrl" text NOT NULL,
+    "createdAt" timestamp without time zone NOT NULL,
+    "createdBy" integer NOT NULL,
+    "visitCount" integer NOT NULL
 );
 
 
+--
+-- Name: public.shortly_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-CREATE TABLE "public.shortly" (
-	"id" serial NOT NULL,
-	"link" character NOT NULL,
-	"shortlylink" integer NOT NULL,
-	"createdAt" TIMESTAMP NOT NULL,
-	"createdBy" integer NOT NULL,
-	"views" integer NOT NULL,
-	CONSTRAINT "shortly_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+CREATE SEQUENCE public."public.shortly_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: public.shortly_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."public.shortly_id_seq" OWNED BY public."public.shortly".id;
+
+
+--
+-- Name: public.users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public."public.users" (
+    id integer NOT NULL,
+    name text NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL
 );
 
 
+--
+-- Name: public.users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."public.users_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 
-ALTER TABLE "shortly" ADD CONSTRAINT "shortly_fk0" FOREIGN KEY ("createdBy") REFERENCES "users"("id");
+--
+-- Name: public.users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."public.users_id_seq" OWNED BY public."public.users".id;
 
 
+--
+-- Name: public.shortly id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."public.shortly" ALTER COLUMN id SET DEFAULT nextval('public."public.shortly_id_seq"'::regclass);
+
+
+--
+-- Name: public.users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."public.users" ALTER COLUMN id SET DEFAULT nextval('public."public.users_id_seq"'::regclass);
+
+
+--
+-- Data for Name: public.shortly; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: public.users; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Name: public.shortly_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."public.shortly_id_seq"', 1, false);
+
+
+--
+-- Name: public.users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."public.users_id_seq"', 1, false);
+
+
+--
+-- Name: public.users public.users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."public.users"
+    ADD CONSTRAINT "public.users_email_key" UNIQUE (email);
+
+
+--
+-- Name: public.shortly shortly_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."public.shortly"
+    ADD CONSTRAINT shortly_pk PRIMARY KEY (id);
+
+
+--
+-- Name: public.users users_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."public.users"
+    ADD CONSTRAINT users_pk PRIMARY KEY (id);
+
+
+--
+-- Name: public.shortly shortly_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."public.shortly"
+    ADD CONSTRAINT shortly_fk0 FOREIGN KEY ("createdBy") REFERENCES public."public.users"(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
 
