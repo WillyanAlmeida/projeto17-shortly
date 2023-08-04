299311d07bacaa503b39132d61f0e38ef3b42da7
Willyan <wkinhaamaral@gmail.com>
2023-08-04 15:21:22 -0300

    feat: get shortly by id

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -12,7 +12,25 @@ export async function postUri(req, res) {
         const newshortly = await db.query(`INSERT INTO shortly ( url, "shortUrl", "createdBy"  ) VALUES
          ($1, $2, $3) RETURNING id, "shortUrl"`, [url, shortUrl, createdBy]);
             
-         res.status(200).send( newshortly.rows[0]);
+         res.status(201).send( newshortly.rows[0]);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function getUribyid(req, res) {
+    const { id } = req.params
+    console.log(req.params)
+
+
+    try {
+        
+         const shortlybyid = await db.query(`SELECT id, "shortUrl", url FROM shortly WHERE id=$1  `, [id]);
+
+         if( shortlybyid.rows.length===0) return  res.sendStatus(404)
+            
+          res.status(200).send( shortlybyid.rows[0]);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -3,7 +3,7 @@ import { validateSchema } from "../middlewares/validateSchema.js";
 
 import { schemasurl } from "../schemas/urlSchemas.js";
 import { validateAuth } from "../middlewares/validateAuth.js";
-import { postUri } from "../controllers/urlController.js";
+import { getUribyid, postUri } from "../controllers/urlController.js";
 
 
 
@@ -11,7 +11,7 @@ const urlRouter = Router()
 
 
 urlRouter.post("/urls/shorten",validateAuth, validateSchema(schemasurl), postUri);
-//urlRouter.post("/signin", validateSchema(schemasignin), signin);
+urlRouter.get("/urls/:id", getUribyid);
 
 
 
