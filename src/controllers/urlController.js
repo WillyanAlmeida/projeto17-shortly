84a048e6d3002df7b7cec046dbaca70b661c924f
Willyan <wkinhaamaral@gmail.com>
2023-08-04 16:27:55 -0300

    feat: delet

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -39,22 +39,34 @@ export async function getUribyid(req, res) {
 
 export async function getUrl(req, res) {
     const { shortUrl } = req.params
-    console.log(req.params)
-
 
     try {
-
         const url = await db.query(`SELECT url, id, "visitCount" FROM shortly WHERE "shortUrl"=$1  `, [shortUrl]);
 
         if (url.rows.length === 0) return res.sendStatus(404)
-        const views = url.rows[0].visitCount+1
+        const views = url.rows[0].visitCount + 1
 
         await db.query(`UPDATE urls SET "visitCount" = $1 WHERE id = $2;`, [views, url.rows[0].id]);
-        
         res.redirect(url.rows[0].url);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
     }
+}
+
+export async function delUrl(req, res) {
+    const {id} = req.params;  
+    const createdBy = Number(res.locals.session)  
+    try {
+        const checkId = await db.query(`SELECT "createdBy" FROM shortly WHERE id= $1 `, [id]);
+        if(checkId.rows.length === 0)return res.sendStatus(404)
+        if(checkId.rows[0].createdBy !== createdBy)return res.sendStatus(401)
+        
+        await db.query(`DELETE FROM shortly WHERE id= $1;`, [id]);
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+
 }
 
--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -3,7 +3,7 @@ import { validateSchema } from "../middlewares/validateSchema.js";
 
 import { schemasurl } from "../schemas/urlSchemas.js";
 import { validateAuth } from "../middlewares/validateAuth.js";
-import { getUribyid, getUrl, postUri } from "../controllers/urlController.js";
+import { delUrl, getUribyid, getUrl, postUri } from "../controllers/urlController.js";
 
 
 
@@ -13,7 +13,7 @@ const urlRouter = Router()
 urlRouter.post("/urls/shorten",validateAuth, validateSchema(schemasurl), postUri);
 urlRouter.get("/urls/:id", getUribyid);
 urlRouter.get("/urls/open/:shortUrl", getUrl);
-
+urlRouter.delete('/urls/:id',validateAuth, delUrl)
 
 
 export default urlRouter
